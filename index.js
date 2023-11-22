b30bc9cd0e81142b0315d3ab023229a9d3ab3fd2
t4sn33m-s4h4t <mdsahat6397@gmail.com>
2023-11-22 22:55:33 +0600

    final

--- index.js
+++ index.js
@@ -2,62 +2,81 @@ const express = require('express');
 const server = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const fs = require('fs').promises; // Use fs.promises for async file operations
 
 server.use(cors({ origin: '*' }));
-
 server.use(bodyParser.json());
 
-// Handle GET request to the home route
+const { MongoClient } = require('mongodb');
+
+const uri = 'mongodb+srv://tasneem:0Ixc82sVBp54aoba@cluster0.sr7echm.mongodb.net/?retryWrites=true&w=majority'; // Replace with your MongoDB Atlas connection string
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
 server.get('/', (req, res) => {
   res.send('Welcome to the home route!');
 });
 
-// Handle POST request to save data to "blogs.txt"
 server.post('/save-blog', async (req, res) => {
   try {
-    const blogData = req.body;
-    const blogDataString = JSON.stringify(blogData);
-    const filePath = 'blogs.txt';
+    const { title, text } = req.body;
+    console.log(req)
+    const blogData = {
+      title: title,
+      text: text,
+    };
 
-    await fs.appendFile(filePath, blogDataString + '\n');
+    await client.connect();
+    const database = client.db('Facebook'); // Replace with your MongoDB database name
+    const collection = database.collection('blogs');
+
+    await collection.insertOne(blogData);
+
     console.log('Data saved successfully');
     res.status(200).send('Data saved successfully');
   } catch (err) {
     console.error(err);
     res.status(500).send('Internal Server Error');
+  } finally {
+    await client.close();
   }
 });
 
-// Handle DELETE request to delete all data
 server.delete('/delete', async (req, res) => {
   try {
-    const filePath = 'blogs.txt';
-    await fs.writeFile(filePath, '', 'utf8');
+    await client.connect();
+    const database = client.db('Facebook');
+    const collection = database.collection('blogs');
+
+    await collection.deleteMany({});
+
     console.log('All data deleted successfully');
     res.status(200).send('All data deleted successfully');
   } catch (err) {
     console.error(err);
     res.status(500).send('Internal Server Error');
+  } finally {
+    await client.close();
   }
 });
 
-// Handle GET request to retrieve all blogs
 server.get('/get-blogs', async (req, res) => {
   try {
-    const filePath = 'blogs.txt';
-    const data = await fs.readFile(filePath, 'utf8');
-    const blogs = data.split('\n').filter(entry => entry.trim() !== '');
-    const parsedBlogs = blogs.map(entry => JSON.parse(entry));
-    res.status(200).json(parsedBlogs);
+    await client.connect();
+    const database = client.db('Facebook');
+    const collection = database.collection('blogs');
+
+    const blogs = await collection.find().toArray();
+
+    res.status(200).json(blogs);
   } catch (err) {
     console.error(err);
     res.status(500).send('Internal Server Error');
+  } finally {
+    await client.close();
   }
 });
 
-const PORT = process.env.PORT || 8080; // Use process.env.PORT for flexibility
-
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
